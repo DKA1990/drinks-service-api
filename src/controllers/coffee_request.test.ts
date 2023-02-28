7e6aa07222c1747c415a4736841dc3483abfdf8d
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-02-28 13:30:52 +0000

    feat: Separation of concerns. Refactored coffee request to MVC

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -5,7 +5,7 @@ test('GET /coffee should return correct object', async () => {
     const res = await request(app)
     .get('/coffee')
     .query({ coffeeName: 'Latte' });
-    
+
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({
         drinkType: 'Coffee',
