caf127bf1e5171758491c6db9fb0193ff208d9ba
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-02-28 13:47:50 +0000

    feat: Implement additional GET coffee tests

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,10 +1,33 @@
 import request from 'supertest';
 import { app } from '../app';
 
-test('GET /coffee should return correct object', async () => {
+test('GET /coffee should return correct object when given name Latte', async () => {
     const res = await request(app)
     .get('/coffee')
     .query({ coffeeName: 'Latte' });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+        drinkType: 'Coffee',
+        name: 'Latte',
+    });
+});
+
+test('GET /coffee should return correct object when given name Americano', async () => {
+    const res = await request(app)
+    .get('/coffee')
+    .query({ coffeeName: 'Americano' });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+        drinkType: 'Coffee',
+        name: 'Americano',
+    });
+});
+
+test('GET /coffee should return correct object when no name given (default to Latte)', async () => {
+    const res = await request(app)
+    .get('/coffee')
 
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({
